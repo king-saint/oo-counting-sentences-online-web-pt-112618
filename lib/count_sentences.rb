4c76e5153ba421af081a4cdf3a491fef680cf1c4
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-18 22:00:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+  else 
+    return false
   end
 
   def question?
-
+      return true if self.end_with?("?")
+    else
+      return false
+      
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+  else
+    return false
   end
 
   def count_sentences
-
+    count = 0
+    new = []
+      new << self.split(%r{(\"!"|\"."|\/|\"?")})
+        if new.length == 0
+          return 0 
+        else
+          new.length
+    end
   end
+  
 end
